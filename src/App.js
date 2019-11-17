c7680b5ce34f8486695e0a4bbbb53aa61dbd435b
Piotrek <piotrek.gorski90@gmail.com>
2019-11-17 11:52:17 +0100

    Add input and button handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -6,43 +6,60 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+/*  */
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onBtnSubmit = () => {
+    console.log("click");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particleParams}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange} 
+          onBtnSubmit={this.onBtnSubmit} 
+          />
+      </div>
+    );
+  }
+}
+
 const particleParams = {
-    particles: {
-    	 color: {
-     	value: '#000000'
-     } ,
-     line_linked: {
+  particles: {
+    color: {
+      value: '#000000'
+    },
+    line_linked: {
       enable: true,
       distance: 150,
       color: '#000000',
-     opacity: 0.4,
+      opacity: 0.4,
       width: 1
     },
-       number: {
-       	value: 300,
-       	density: {
-       		enable: true,
-       		value_area: 8000
-       	}
-       }             				
-   }
-} 
-
-
-class App extends Component {
-	render(){
-    return (
-        <div className="App">
-          <Particles className='particles'
-            params={particleParams}
-            />
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm />
-        </div>
-  );
-}
+    number: {
+      value: 300,
+      density: {
+        enable: true,
+        value_area: 8000
+      }
+    }
+  }
 }
 
 export default App;
